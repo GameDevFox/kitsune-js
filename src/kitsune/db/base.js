d508d6f54d80be85679f5c0929a77f875b48bb4e
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-09-21 23:17:32 -0500

    Make changes to kistune/db/init

--- src/kitsune/db/base.js
+++ src/kitsune/db/base.js
@@ -1,14 +1,22 @@
-export function createTable(name, ...cols) {
-	return new Promise(function(resolve, reject) {
-		var query = "CREATE TABLE " + name + "(";
-		query += cols.join(", ");
-		query += ");";
-		log.query(query);
-		db.run(query, function(err) {
-			if(err)
-				reject(err);
+export default function buildBase(sqliteDB) {
 
-			resolve(name);
+	var base = {};
+
+	var createTable = function(name, ...cols) {
+		return new Promise(function(resolve, reject) {
+			var query = "CREATE TABLE " + name + "(";
+			query += cols.join(", ");
+			query += ");";
+
+			sqliteDB.run(query, function(err) {
+				if(err)
+					reject(err);
+
+				resolve(name);
+			});
 		});
-	});
+	};
+	base.createTable = createTable;
+
+	return base;
 }
