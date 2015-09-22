63872ae45d625d675d661761a42a3001a49fb029
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-09-22 16:33:48 -0500

    Actually converting script now

--- src/kitsune/db/base.js
+++ src/kitsune/db/base.js
@@ -2,9 +2,9 @@ export default function buildBase(sqliteDB) {
 
 	var base = {};
 
-	var createTable = function(name, ...cols) {
+	var create = function(name, ...cols) {
 		return new Promise(function(resolve, reject) {
-			var query = "CREATE TABLE " + name + "(";
+			var query = "CREATE TABLE " + name + " (";
 			query += cols.join(", ");
 			query += ");";
 
@@ -16,7 +16,20 @@ export default function buildBase(sqliteDB) {
 			});
 		});
 	};
-	base.createTable = createTable;
+	base.create = create;
+
+	var alias = function(tableId, alias) {
+		return new Promise(function(resolve, reject) {
+			var query = `CREATE VIEW "${alias}" AS SELECT * FROM t${tableId};`;
+			sqliteDB.run(query, function(err) {
+				if(err)
+					reject(err);
+
+				resolve(alias);
+			});
+		});
+	};
+	base.alias = alias;
 
 	return base;
 }
--- src/kitsune/db/init.js
+++ src/kitsune/db/init.js
@@ -2,26 +2,30 @@
 import _ from "lodash";
 import sqlite3 from "sqlite3";
 
+import { log } from "katana/system";
 import { ids, tables } from "./index";
 import buildBase from "./base";
 
 export function init() {
 
-	var revLookup = {};
-	_.each(tables, function(key, value) {
-		revLookup[value] = key;
-	});
-
 	var sqliteDB = new sqlite3.Database("data/data.db");
 	var base = buildBase(sqliteDB);
 
-	var createTable = base.createTable;
+	var create = base.create;
+	var alias = base.alias;
 
-	createTable("core", "id TEXT", "name TEXT")
-		.then(createTable(ids.node, "id TEXT PRIMARY KEY", "type TEXT"))
-		.then(createTable(ids.table, "id TEXT"))
-		.then(createTable(ids.relationship, "id TEXT", "head TEXT", "tail TEXT", "type TEXT"))
-		.then(createTable(ids.string, "id TEXT", "string TEXT"));
+	sqliteDB.serialize(function() {
+		create("core", "id TEXT", "name TEXT");
+		create("t"+ids.node, "id TEXT PRIMARY KEY", "type TEXT");
+		create("t"+ids.table, "id TEXT");
+		create("t"+ids.relationship, "id TEXT", "head TEXT", "tail TEXT", "type TEXT");
+		create("t"+ids.string, "id TEXT", "string TEXT");
+
+		_.each(tables, function(table) {
+			var tableName = _.findKey(ids, function(id) { return id == table; });
+			alias(table, tableName).catch(log);
+		});
+	});
 }
 
 init();
