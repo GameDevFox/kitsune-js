242624d1df1b453aaff57b8624740799d42b56e6
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-11-20 20:55:00 -0600

    Clean up "kitsune/db/init"

--- src/kitsune/db/init.js
+++ src/kitsune/db/init.js
@@ -11,18 +11,15 @@ export default function init(dbSys) {
 	var { alias, create, view } = dbSys;
 
 	// rel database
-	var p = create("t"+ids.relationship, "id TEXT", "head TEXT", "tail TEXT")
+	return create("t"+ids.relationship, "id TEXT", "head TEXT", "tail TEXT")
 			.then(alias("t"+ids.relationship, "relationship"))
-			.then(alias("relationship", "rel"));
-
+			.then(alias("relationship", "rel"))
 	// Add ids view
-	p.then(view("ids", "SELECT id FROM rel UNION SELECT head FROM rel UNION SELECT tail FROM rel"));
-
+	.then(view("ids", "SELECT id FROM rel UNION SELECT head FROM rel UNION SELECT tail FROM rel"))
 	// str database
-	p.then(create("t"+ids.string, "id TEXT", "string TEXT").catch(console.error))
-		.then(alias("t"+ids.string, "string"));
-
-	return p.catch(console.error);
+	.then(create("t"+ids.string, "id TEXT", "string TEXT").catch(console.error))
+		.then(alias("t"+ids.string, "string"))
+	.catch(console.error);
 
 	// TODO: The new equivalent of these
 	// echo "INSERT INTO core VALUES (\"$id\", \"$name\");"
