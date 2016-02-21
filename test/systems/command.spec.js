e27a36a314b1c9fcc022357266a681b68fc80424
Edward Nicholes Jr <GameDevFox@gmail.com>
2016-02-21 14:02:39 -0600

    Remove notes

--- test/systems/command.spec.js
+++ test/systems/command.spec.js
@@ -31,37 +31,6 @@ describe("kitsune/command", function() {
 					expect(result).to.equal(123);
 				})
 				.then(done, done);
-
-			// Random number between x and y
-			// ============================
-			// (x, y) => {
-			// 	let rand = Math.random();
-			// 	let range = y - x;
-
-			// 	console.log(range);
-			// 	rand = rand * range;
-			// 	rand = rand + x;
-			// 	return rand;
-			// }
-
-			// Deps
-			// ==========================
-			// x
-			// y
-			// Math.random
-			// console.log
-			// - (minus operator)
-			// * (multiply operator)
-			// + (plus operator)
-
-			// Generated code
-			// =========================
-			// let v1 = f12387348297239();
-			// let v2 = f83748372438324(p1, p2); // subtract
-			// f34283749283423(v2);
-			// v1 = f45837593487543(v1. v2); // multiply
-			// v1 = f43587345943875(v1, p1); // add
-			// return v1;
 		});
 	});
 });
