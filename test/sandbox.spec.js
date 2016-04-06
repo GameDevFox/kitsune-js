61d7c81b0160d21e8d89f8d76cdc4b2ddb98f270
Edward Nicholes Jr <GameDevFox@gmail.com>
2016-04-05 23:00:51 -0500

    Make system-loader syncronous

--- test/sandbox.spec.js
+++ test/sandbox.spec.js
@@ -8,65 +8,75 @@ import systemLoader from "kitsune-core/31d21eb2620a8f353a250ad2edd4587958faf3b1"
 let loader = bind(systemLoader, { path: "kitsune-core" });
 
 describe("sandbox", function() {
-    it.only("should have sand in it", function(done) {
-        let systemIds = ["fe60fc76f26f8dce6c5f68bbb0ea0c51efef3dff", // loki-collection
-                         "a73b64eba9daa07051815ca7151ba009789616e2", // graph-autoPut
-                         "6c877bef62bc8f57eb55265c62e75b36515ef458", // graph-assign
-                         "4163d1cd63d3949b79c37223bd7da04ad6cd36c8", // graph-factor
-                         "b7916f86301a6bc2af32f402f6515809bac75b03", // graph-listNodes
-                         "8f8b523b9a05a55bfdffbf14187ecae2bf7fe87f", // string-autoPut
-                         "ddfe7d402ff26c18785bcc899fa69183b3170a7d", // name
-                        ];
+    it.only("should have sand in it", function() {
 
-        let promises = systemIds.map(id => loader({ id }));
-        Promise.all(promises)
-            .then(systems => {
-                let [
-                    lokiColl,
-                    graphAutoPut,
-                    graphAssign,
-                    graphFactor,
-                    graphListNodes,
-                    stringAutoPut,
-                    name
-                ] = systems;
+        let data;
+        try {
+            let json = fs.readFileSync("data.json");
+            data = JSON.parse(json);
+        } catch(e) {
+            data = {
+                graph: [],
+                string: []
+            };
+        }
 
-                var { graph, string } = readData(lokiColl);
+        let systemIds = [
+            "fe60fc76f26f8dce6c5f68bbb0ea0c51efef3dff", // loki-collection
+            "a73b64eba9daa07051815ca7151ba009789616e2", // graph-autoPut
+            "6c877bef62bc8f57eb55265c62e75b36515ef458", // graph-assign
+            "4163d1cd63d3949b79c37223bd7da04ad6cd36c8", // graph-factor
+            "b7916f86301a6bc2af32f402f6515809bac75b03", // graph-listNodes
+            "8f8b523b9a05a55bfdffbf14187ecae2bf7fe87f", // string-autoPut
+            "ddfe7d402ff26c18785bcc899fa69183b3170a7d", // name
+        ];
 
-                // Build systems
-                graph.autoPut = bind(graphAutoPut, { graphPut: graph.put });
-                graph.assign = bind(graphAssign, { graphAutoPut: graph.autoPut });
-                graph.factor = bind(graphFactor, { graphFind: graph.find });
-                graph.listNodes = bind(graphListNodes, { graphFind: graph.find });
-                string.autoPut = bind(stringAutoPut, { stringPut: string.put });
-                name = bind(name, { stringAutoPut: string.autoPut, graphAssign: graph.assign });
+        let systems = systemIds.map(id => loader({ id }));
 
-                // Execute systems
-                var nodes = graph.listNodes();
-                console.log(nodes.sort());
+        let [
+            lokiColl,
+            graphAutoPut,
+            graphAssign,
+            graphFactor,
+            graphListNodes,
+            stringAutoPut,
+            name
+        ] = systems;
 
-                // printTable(graph.find);
+        var { graph, string } = readData(data, lokiColl);
 
-                // let group = graph.find({ where: { head: "66564ec14ed18fb88965140fc644d7b813121c78" } });
-                // let systemFiles = group.map(x => x.tail).sort();
-                // console.log("=== System files ===");
-                // console.log(systemFiles);
+        // Build systems
+        graph.autoPut = bind(graphAutoPut, { graphPut: graph.put });
+        graph.assign = bind(graphAssign, { graphAutoPut: graph.autoPut });
+        graph.factor = bind(graphFactor, { graphFind: graph.find });
+        graph.listNodes = bind(graphListNodes, { graphFind: graph.find });
+        string.autoPut = bind(stringAutoPut, { stringPut: string.put });
+        name = bind(name, { stringAutoPut: string.autoPut, graphAssign: graph.assign });
 
-                // let systemMap = graph.factor({ head: systemFiles, type: "f1830ba2c84e3c6806d95e74cc2b04d99cd269e0" });
-                // systemMap.forEach(value => {
-                //     var names = string.find({ where: { id: value.tail } });
-                //     value.name = names[0].string;
-                // });
-                // systemMap = systemMap.map(value => ({ head: value.head, name: value.name }));
-                // systemMap = _.sortBy(systemMap, ["head"]);
-                // console.log(systemMap);
+        // Execute systems
+        // var nodes = graph.listNodes();
+        // console.log(nodes.sort());
 
-                // Sort and save Data
-                let graphData = _.sortBy(graph.find(), ["head", "tail"]);
-                let stringData = _.sortBy(string.find(), ["string"]);
-                writeData({ graph: graphData, string: stringData });
-            })
-            .then(done, done);
+        // printTable(graph.find);
+
+        let group = graph.find({ where: { head: "66564ec14ed18fb88965140fc644d7b813121c78" } });
+        let systemFiles = group.map(x => x.tail).sort();
+        console.log("=== System files ===");
+        console.log(systemFiles);
+
+        let systemMap = graph.factor({ head: systemFiles, type: "f1830ba2c84e3c6806d95e74cc2b04d99cd269e0" });
+        systemMap.forEach(value => {
+            var names = string.find({ where: { id: value.tail } });
+            value.name = names[0].string;
+        });
+        systemMap = systemMap.map(value => ({ head: value.head, name: value.name }));
+        systemMap = _.sortBy(systemMap, ["head"]);
+        console.log(systemMap);
+
+        // Sort and save Data
+        let graphData = _.sortBy(graph.find(), ["head", "tail"]);
+        let stringData = _.sortBy(string.find(), ["string"]);
+        writeData({ graph: graphData, string: stringData });
     });
 });
 
@@ -95,18 +105,7 @@ function logLoki(data) {
     console.log(json);
 }
 
-function readData(lokiColl) {
-    let data;
-    try {
-        let json = fs.readFileSync("data.json");
-        data = JSON.parse(json);
-    } catch(e) {
-        data = {
-            graph: [],
-            string: []
-        };
-    }
-
+function readData(data, lokiColl) {
     let controls = _.mapValues(data, collData => {
         let coll = new Collection();
         var control = _.mapValues(lokiColl(), (func, name) => {
