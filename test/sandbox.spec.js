833b4634299d3001e4aea25a4f472b1333e172ab
Edward Nicholes Jr <GameDevFox@gmail.com>
2016-05-06 17:45:11 -0500

    Auto load file systems

--- test/sandbox.spec.js
+++ test/sandbox.spec.js
@@ -23,6 +23,9 @@ describe("sandbox", function() {
         let lokiPut = loader({ id: "f45ccdaba9fdca2234be7ded1a5578dd17c2374e" }); // loki-put
         let lokiFind = loader({ id: "30dee1b715bcfe60afeaadbb0e3e66019140686a" }); // loki-find
 
+        let graphFactor = loader({ id: "4163d1cd63d3949b79c37223bd7da04ad6cd36c8" }); // graph-factor
+        let getNames = loader({ id: "81e0ef7e2fae9ccc6e0e3f79ebf0c9e14d88d266" }); // getNames
+
         // INIT DATA SYSTEMS
         var data = { graph: graphData(), string: stringData() };
 
@@ -46,51 +49,52 @@ describe("sandbox", function() {
 
         let { graph, string } = data;
 
-        // INIT SYSTEMS
-        let systemIds = [
-            "2ebd8d9fff28833dab44f086d4692fb888525fc8", // graph-remove
-            "a73b64eba9daa07051815ca7151ba009789616e2", // graph-autoPut
-            "6c877bef62bc8f57eb55265c62e75b36515ef458", // graph-assign
-            "4163d1cd63d3949b79c37223bd7da04ad6cd36c8", // graph-factor
-            "b7916f86301a6bc2af32f402f6515809bac75b03", // graph-listNodes
-            "8f8b523b9a05a55bfdffbf14187ecae2bf7fe87f", // string-autoPut
-            "ddfe7d402ff26c18785bcc899fa69183b3170a7d", // name
-            "81e0ef7e2fae9ccc6e0e3f79ebf0c9e14d88d266", // getNames
-            "d2f544f574dae26adb5ed3ee70c71e302b2575fa", // is-in-collection
-            "4bea815e7814aa415569ecd48e5733a19e7777db", // describe-node
-            "a3fd8e7c0d51f13671ebbb6f9758833ff6120b42", // is-in-group
-            "90184a3d0c84658aac411637f7442f80b3fe0040", // and-is
-        ];
+        // Auto load systems
+        graph.factor = bind({ func: graphFactor, params: { graphFind: graph.find }});
+        getNames = bind({ func: getNames, params: { graphFactor: graph.factor, stringFind: string.find }});
 
-        let systems = systemIds.map(id => loader({ id }));
+        let group = graph.find({ where: { head: "66564ec14ed18fb88965140fc644d7b813121c78" } });
+        let groupIds = _.map(group, "tail");
 
-        let [
+        let systems = {};
+        let systemsByName = {};
+        groupIds.forEach(id => {
+            let names = getNames({ node: id });
+            let system = loader({ id });
+
+            systems[id] = system;
+
+            names.forEach(name => {
+                let camelName = name
+                        .replace(/-(.)/g, capture => capture.toUpperCase())
+                        .replace(/-/g, '');
+                systemsByName[camelName] = system;
+            });
+        });
+
+        // Build systems
+        let {
             lokiRemove,
             graphAutoPut,
             graphAssign,
-            graphFactor,
             graphListNodes,
             stringAutoPut,
             name,
-            getNames,
             isInCollection,
-            describe,
+            describeNode,
             isInGroup,
             andIs
-        ] = systems;
+        } = systemsByName;
 
-        // Build systems
         graph.remove = bind({ func: lokiRemove, params: { db: graph.coll }});
         graph.autoPut = bind({ func: graphAutoPut, params: { graphPut: graph.put }});
         graph.assign = bind({ func: graphAssign, params: { graphAutoPut: graph.autoPut }});
-        graph.factor = bind({ func: graphFactor, params: { graphFind: graph.find }});
         graph.listNodes = bind({ func: graphListNodes, params: { graphFind: graph.find }});
 
         string.remove = bind({ func: lokiRemove, params: { db: string.coll }});
         string.autoPut = bind({ func: stringAutoPut, params: { stringFind: string.find, stringPut: string.put }});
 
         name = bind({ func: name, params: { stringAutoPut: string.autoPut, graphAssign: graph.assign }});
-        getNames = bind({ func: getNames, params: { graphFactor: graph.factor, stringFind: string.find }});
 
         let createSystemFile = bind({ func: _createSystemFile, params: { graphAutoPut: graph.autoPut, nameFn: name }});
         let isEdge = bind({ func: isInCollection, params: { collFind: graph.find }});
@@ -98,18 +102,13 @@ describe("sandbox", function() {
 
         // Execute systems
         // createSystemFile({ name: "loki-put" });
-        // name({ node: "fe60fc76f26f8dce6c5f68bbb0ea0c51efef3dff", name: "old-loki-collection" });
-
-        // console.log(systemIds.length);
-        // console.log(systems);
-        // let group = graph.find({ where: { head: "66564ec14ed18fb88965140fc644d7b813121c78" } });
-        // console.log(_.map(group, "tail"));
+        // name({ node: "d2f544f574dae26adb5ed3ee70c71e302b2575fa", name: "another-name" });
 
         let coreNodes = fs.readdirSync("node_modules/kitsune-core");
         // REPORTS //
         {
             // Node description report
-            // nodeDescReport({ bind, isInGroup, graph, andIs, isEdge, isString, describe });
+            // nodeDescReport({ bind, isInGroup, graph, andIs, isEdge, isString, describeNode });
 
             // System file report
             systemFileReport({ coreNodes, graph, getNames });
@@ -144,7 +143,7 @@ describe("sandbox", function() {
 });
 
 // Report functions
-function nodeDescReport({ bind, isInGroup, graph, andIs, isEdge, isString, describe }) {
+function nodeDescReport({ bind, isInGroup, graph, andIs, isEdge, isString, describeNode }) {
     let isCoreNode = bind({ func: isInGroup, params: { graphFind: graph.find, group: "7f82d45a6ffb5c345f84237a621de35dd8b7b0e3" }});
     let isSystemFile = bind({ func: isInGroup, params: { graphFind: graph.find, group: "66564ec14ed18fb88965140fc644d7b813121c78" }});
     let isInNameGroup = bind({ func: isInGroup, params: { graphFind: graph.find, group: "f1830ba2c84e3c6806d95e74cc2b04d99cd269e0" }});
@@ -154,7 +153,7 @@ function nodeDescReport({ bind, isInGroup, graph, andIs, isEdge, isString, descr
     let nodeList = graph.listNodes();
 
     nodeList.forEach(node => {
-        let types = describe({ node: node, types: typeMap });
+        let types = describeNode({ node: node, types: typeMap });
         console.log(node+" => "+JSON.stringify(types));
     });
 }
